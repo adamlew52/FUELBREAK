c42b6e6c0934d9f57c2d895d1cf7a1db806cd62a
Adam Lewis <95544948+adamlew52@users.noreply.github.com>
2026-03-28 18:15:36 -0600

    Initial Commit

---
+++ FUELBREAK/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FUELBREAK
+//
+//  Created by alew on 3/28/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ FUELBREAKTests/FUELBREAKTests.swift
@@ -0,0 +1,16 @@
+//
+//  FUELBREAKTests.swift
+//  FUELBREAKTests
+//
+//  Created by alew on 3/28/26.
+//
+
+import Testing
+
+struct FUELBREAKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
